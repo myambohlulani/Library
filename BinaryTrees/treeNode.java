3624a0f9e62be2c3584714f5758509197b93fcaa
Koketso <koketsogaowelwe2@gmail.com>
2026-03-12 09:03:00 +0200

    red black

---
+++ BinaryTrees/treeNode.java
@@ -0,0 +1,20 @@
+package BinaryTrees;
+
+import java.awt.Color;
+
+
+public class treeNode<T extends Comparable<T>> {
+
+    public T data;
+    public Color color = Color.RED;
+    public treeNode<T> left;
+    public treeNode<T> right;
+    public treeNode<T> parent;
+
+    public treeNode(T data, treeNode<T> parent) {
+        this.data = data;
+        this.parent = parent;
+
+    }
+
+}
