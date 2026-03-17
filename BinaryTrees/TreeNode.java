fdc46d543f6dc27eb174a881fdc7dcf47d9162d4
Hlulani Myambo <mymhlu001@myuct.ac.za>
2026-03-17 16:40:47 +0200

    feat: Created an enum for color\nfeat: Added toString to help with debugging\nfeat: enforcing the color to be red into each and every node being added regardless of what

--- BinaryTrees/TreeNode.java
+++ BinaryTrees/TreeNode.java
@@ -1,17 +1,27 @@
 package BinaryTrees;
 
-import java.awt.Color;
-
-
-public class treeNode<T extends Comparable<T>> {
-
+/**
+ * Implementation of RBT Node
+ *
+ * @author
+ * @version
+ */
+public class TreeNode<T extends Comparable<T>> {
     public T data;
-    public Color color = Color.RED;
-    public treeNode<T> left, right;
+    public Color color;
+    public TreeNode<T> left, right;
 
-    public treeNode(T data) {
+    public TreeNode(T data) {
         this.data = data;
-
+        this.color = Color.RED;
     }
 
+    enum Color {
+        RED, BLACK
+    }
+
+    @Override
+    public String toString() {
+        return "node={data=[" + this.data + "], color=[" + this.color + "]}";
+    }
 }
